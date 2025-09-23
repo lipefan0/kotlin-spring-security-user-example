802c16556fa89680af84df9b064d758ac964e467
Felipe Oliveira <lipefan0@gmail.com>
2025-09-23 11:51:46 -0300

    initial commit

---
+++ src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/KotlinSpringSecurityUserExampleApplication.kt
@@ -0,0 +1,11 @@
+package br.com.contis.kotlin_spring_security_user_example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSecurityUserExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringSecurityUserExampleApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/contis/kotlin_spring_security_user_example/KotlinSpringSecurityUserExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.contis.kotlin_spring_security_user_example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSecurityUserExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
