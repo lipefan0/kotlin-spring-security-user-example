d2790cfef85a6fe9634ef7a9950ae9517c187e31
Felipe Oliveira <lipefan0@gmail.com>
2025-09-24 10:15:27 -0300

    adiciona configuracoes e rotas de registro e autenticao de usuario

--- src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/controller/UserController.kt
+++
@@ -1,4 +0,0 @@
-package br.com.contis.kotlin_spring_security_user_example.controller
-
-class UserController {
-}
--- src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/dtos/RegisterDTO.kt
+++ src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/dtos/RegisterDTO.kt
@@ -1,4 +1,10 @@
 package br.com.contis.kotlin_spring_security_user_example.dtos
 
-class RegisterDTO {
-}
+import br.com.contis.kotlin_spring_security_user_example.entity.RoleEnum
+
+data class RegisterDTO(
+    val name: String,
+    val email: String,
+    val password: String,
+    val role: RoleEnum = RoleEnum.USER
+)
