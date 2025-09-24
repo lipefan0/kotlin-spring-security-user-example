8200d2ef6424a86f45f40e834774e944e1446851
Felipe Oliveira <lipefan0@gmail.com>
2025-09-24 09:35:31 -0300

    adiciona repository de usuario com a funcao de procurar usuario por email

--- src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/repository/UserRepository.kt
+++ src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/repository/UserRepository.kt
@@ -1,4 +1,11 @@
 package br.com.contis.kotlin_spring_security_user_example.repository
 
-interface UserRepository {
+import br.com.contis.kotlin_spring_security_user_example.entity.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+interface UserRepository: JpaRepository<UserEntity, Long> {
+    fun findByEmail(email: String): Optional<UserEntity>
 }
