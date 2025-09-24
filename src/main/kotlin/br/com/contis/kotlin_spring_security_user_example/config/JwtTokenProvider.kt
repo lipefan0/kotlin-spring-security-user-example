a2c32964e8987bcf628b8ffd4e3be3b9f1a6cc16
Felipe Oliveira <lipefan0@gmail.com>
2025-09-24 09:54:06 -0300

    adiciona configuracoes de jwt e spring security

--- src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/config/JwtTokenProvider.kt
+++ src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/config/JwtTokenProvider.kt
@@ -1,4 +1,52 @@
 package br.com.contis.kotlin_spring_security_user_example.config
 
-class JwtTokenProvider {
+import br.com.contis.kotlin_spring_security_user_example.entity.UserEntity
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.Base64
+import java.util.Date
+import javax.crypto.SecretKey
+
+@Component
+class JwtTokenProvider(
+    @Value("\${app.secret}") private val secret: String,
+    @Value("\${app.expiration-in-ms}") private val expirationInMs: Long
+) {
+    private val key: SecretKey = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret))
+
+    fun generateToken(user: UserEntity): String {
+        val now = Date()
+        val expirationDate = Date(now.time + expirationInMs)
+
+        return Jwts.builder()
+            .subject(user.email)
+            .issuedAt(now)
+            .expiration(expirationDate)
+            .signWith(key)
+            .compact()
+    }
+
+    fun getEmailFromToken(token: String): String {
+        val claims: Claims = Jwts.parser()
+            .verifyWith(key)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+        return claims.subject
+    }
+
+    fun validateToken(token: String): Boolean {
+        try {
+            Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+            return true
+        } catch (ex: Exception) {
+        }
+        return false
+    }
 }
--- src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/service/CustomUserDetailsService.kt
+++ src/main/kotlin/br/com/contis/kotlin_spring_security_user_example/service/CustomUserDetailsService.kt
@@ -1,4 +1,18 @@
 package br.com.contis.kotlin_spring_security_user_example.service
 
-class CustomUserDetailsService {
+import br.com.contis.kotlin_spring_security_user_example.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+): UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails? {
+        return userRepository.findByEmail(username)
+            .orElseThrow { UsernameNotFoundException("Usuario não encontrado com o email: $username") }
+
+    }
 }
